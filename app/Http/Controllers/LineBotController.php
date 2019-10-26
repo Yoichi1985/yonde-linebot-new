dbccdd61c6eb3c3e1abed0baa278017695ace7bc
Yoichi1985 <tasaka4001@gmail.com>
2019-10-26 16:29:27 +0900

    add img

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -41,6 +41,8 @@ class LineBotController extends Controller
 
             $replyToken = $event->getReplyToken();
             $replyText = $event->getText();
+            $replyImage = $event->getImage();
+
 
             if (is_numeric($replyText)) {
                 $lineBot->replyImage("https://3.bp.blogspot.com/-vQSPQf-ytsc/T3K7QM3qaQI/AAAAAAAAE-s/6SB2q7ltxwg/s1600/omikuji_daikichi.png
